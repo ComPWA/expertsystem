d986be89f4ca5ca680088d1f838dbb84294ea879
Remco de Boer <redeboer@gmx.com>
2020-05-13 15:18:23 +0200

    style: fix linter errors

--- expertsystem/amplitude/abstractgenerator.py
+++ expertsystem/amplitude/abstractgenerator.py
@@ -1,25 +1,53 @@
-from abc import ABC, abstractmethod
+"""Collection of abstract interfaces for amplitude model generation."""
+
+
+from abc import ABC
+from abc import abstractmethod
+
+from typing import Tuple
 
 
 class AbstractAmplitudeNameGenerator(ABC):
-    @abstractmethod
-    def generate_unique_amplitude_name(self, graph, node_id):
-        pass
+    """Interface for creating strings for the :class:`StateTransitionGraph`."""
 
     @abstractmethod
-    def generate_amplitude_coefficient_infos(self, graph):
-        pass
+    def generate_unique_amplitude_name(self, graph: list, node_id: int) -> str:
+        """
+        Generate a unique name for the amplitude.
+
+        The name corresponds to the given none give in
+        :class:`StateTransitionGraph`. If ``node_id`` is given, it generates a
+        unique name for the partial amplitude corresponding to the interaction
+        node of the given :class:`StateTransitionGraph`.
+        """
 
     @abstractmethod
-    def _generate_amplitude_coefficient_names(self, graph, node_id):
+    def generate_amplitude_coefficient_infos(self, graph: list) -> dict:
+        """Generate coefficient info for a sequential amplitude graph."""
+
+    @abstractmethod
+    def _generate_amplitude_coefficient_names(
+        self, graph: list, node_id: int
+    ) -> Tuple[str, str]:
         pass
 
 
 class AbstractAmplitudeGenerator(ABC):
-    @abstractmethod
-    def generate(self, graphs):
-        pass
+    """Abstract interface that handles amplitude model generation."""
 
     @abstractmethod
-    def write_to_file(self, filename):
-        pass
+    def generate(self, graphs: list) -> None:
+        """
+        Generate an amplitude model from a given `list` of graphs.
+
+        The result is stored internally.
+        """
+
+    @abstractmethod
+    def write_to_file(self, filename: str) -> None:
+        """
+        Write generated amplitude model to a recipe file.
+
+        This function can only be called to be called *after*
+        `~.AbstractAmplitudeGenerator.generate`.
+        """
