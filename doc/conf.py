451dded59e3a528018cfa926539243f12b58642b
Leonard <leo-wol@web.de>
2020-05-29 11:07:56 +0200

    Create doc/usage dir

--- doc/conf.py
+++ doc/conf.py
@@ -14,6 +14,7 @@ import subprocess
 print("Copy example notebook files")
 # Remove old notebooks
 PATH_TARGET = "usage"
+os.makedirs(PATH_TARGET, exist_ok=True)
 for root, _, files in os.walk(PATH_TARGET):
     for notebook in files:
         if notebook.endswith(".ipynb"):
