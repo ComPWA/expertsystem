e0b389b74280c915f31c5c1ea69a21a051a26109
Remco de Boer <redeboer@gmx.com>
2020-05-31 22:30:15 +0200

    test: test unequal ParticleDatabases

--- tests/particle/test_database.py
+++ tests/particle/test_database.py
@@ -1,5 +1,10 @@
 import pytest
 
+from expertsystem.state.particle import (
+    Particle,
+    QuantumNumbers,
+    Spin,
+)
 from expertsystem.state.particle import ParticleDatabase
 
 
@@ -59,3 +64,30 @@ class TestParticleDatabase:
         with pytest.raises(NotImplementedError):
             assert database_xml == 0
         assert database_xml == database_yml
+
+        particle = database_xml["gamma"]
+        corrupted_particle = Particle(
+            name=particle.name,
+            pid=particle.pid,
+            mass=particle.mass,
+            width=particle.width,
+            quantum_numbers=QuantumNumbers(
+                charge=particle.quantum_numbers.charge,
+                spin=Spin(2.5, -1.5),  # only one different, nested value
+                isospin=particle.quantum_numbers.isospin,
+                parity=particle.quantum_numbers.parity,
+                parity_c=particle.quantum_numbers.parity_c,
+                parity_g=particle.quantum_numbers.parity_g,
+                strangeness=particle.quantum_numbers.strangeness,
+                charmness=particle.quantum_numbers.charmness,
+                bottomness=particle.quantum_numbers.bottomness,
+                topness=particle.quantum_numbers.topness,
+                baryon_number=particle.quantum_numbers.baryon_number,
+                ln_electron=particle.quantum_numbers.ln_electron,
+                ln_muon=particle.quantum_numbers.ln_muon,
+                ln_tau=particle.quantum_numbers.ln_tau,
+            ),
+        )
+        database_xml.add_particle(corrupted_particle)
+        assert len(database_xml) == len(database_yml)
+        assert database_xml != database_yml
