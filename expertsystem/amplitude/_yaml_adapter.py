6bb24ecb197783ed1ac990b406ea2a7195910f4a
Remco de Boer <redeboer@gmx.com>
2020-06-15 14:06:59 +0200

    fix: hide implementation details YAML adapter

--- expertsystem/amplitude/_yaml_adapter.py
+++ expertsystem/amplitude/_yaml_adapter.py
@@ -10,6 +10,24 @@ from typing import (
 )
 
 
+def to_kinematics_dict(recipe: Dict[str, Any]) -> Dict[str, Any]:
+    kinematics_xml = recipe["HelicityKinematics"]
+    initialstate_xml = kinematics_xml["InitialState"]["Particle"]
+    initial_state_yml = list()
+    for state_def_xml in initialstate_xml:
+        state_def_yml = {
+            "Particle": state_def_xml["Name"],
+            "ID": int(state_def_xml["Id"]),
+        }
+        initial_state_yml.append(state_def_yml)
+    kinematics_yaml = {
+        "Type": "Helicity",
+        "InitialState": _to_state_list(kinematics_xml, "InitialState"),
+        "FinalState": _to_state_list(kinematics_xml, "FinalState"),
+    }
+    return kinematics_yaml
+
+
 def to_particle_list_dict(recipe: Dict[str, Any]) -> Dict[str, Any]:
     particle_list_xml = recipe["ParticleList"]["Particle"]
     particle_list_yml = dict()
@@ -17,25 +35,25 @@ def to_particle_list_dict(recipe: Dict[str, Any]) -> Dict[str, Any]:
         name = str(xml_particle["Name"])
         pid = int(xml_particle["Pid"])
         parameters = xml_particle["Parameter"]
-        mass = to_parameter(parameters)
+        mass = _to_parameter(parameters)
         quantum_numbers = list(xml_particle["QuantumNumber"])
         decay_info = xml_particle.get("DecayInfo", None)
 
         qn_key_map: Dict[str, Tuple[str, Callable]] = {
-            "Charge": ("Charge", to_scalar),
-            "Spin": ("Spin", to_spin),
-            "IsoSpin": ("IsoSpin", to_spin),
-            "Parity": ("Parity", to_scalar),
-            "Cparity": ("Cparity", to_scalar),
-            "Gparity": ("Gparity", to_scalar),
-            "Strangeness": ("Strangeness", to_scalar),
-            "Charm": ("Charm", to_scalar),
-            "Bottomness": ("Bottomness", to_scalar),
-            "Topness": ("Topness", to_scalar),
-            "ElectronLN": ("ElectronLN", to_scalar),
-            "MuonLN": ("MuonLN", to_scalar),
-            "TauLN": ("TauLN", to_scalar),
-            "BaryonNumber": ("BaryonNumber", to_scalar),
+            "Charge": ("Charge", _to_scalar),
+            "Spin": ("Spin", _to_spin),
+            "IsoSpin": ("IsoSpin", _to_spin),
+            "Parity": ("Parity", _to_scalar),
+            "Cparity": ("Cparity", _to_scalar),
+            "Gparity": ("Gparity", _to_scalar),
+            "Strangeness": ("Strangeness", _to_scalar),
+            "Charm": ("Charm", _to_scalar),
+            "Bottomness": ("Bottomness", _to_scalar),
+            "Topness": ("Topness", _to_scalar),
+            "ElectronLN": ("ElectronLN", _to_scalar),
+            "MuonLN": ("MuonLN", _to_scalar),
+            "TauLN": ("TauLN", _to_scalar),
+            "BaryonNumber": ("BaryonNumber", _to_scalar),
         }
         yaml_qn_dict = {"Spin": 0.0, "Charge": 0.0}
         for quantum_number in quantum_numbers:
@@ -50,13 +68,13 @@ def to_particle_list_dict(recipe: Dict[str, Any]) -> Dict[str, Any]:
             parameters = decay_info.get("Parameter", list())
             for parameter in parameters:
                 if parameter["Type"] == "Width":
-                    particle_yml["Width"] = to_parameter(parameter)
+                    particle_yml["Width"] = _to_parameter(parameter)
         particle_yml["QuantumNumbers"] = yaml_qn_dict
         particle_list_yml[name] = particle_yml
     return particle_list_yml
 
 
-def to_scalar(
+def _to_scalar(
     definition: Dict[str, str], key: str = "Value"
 ) -> Union[float, int]:
     value = float(definition[key])
@@ -65,8 +83,10 @@ def to_scalar(
     return value
 
 
-def to_parameter(definition: Dict[str, Any]) -> Union[float, Dict[str, float]]:
-    """Used for extracting Mass and Width keys."""
+def _to_parameter(
+    definition: Dict[str, Any]
+) -> Union[float, Dict[str, float]]:
+    """Use for extracting Mass and Width keys."""
     value = float(definition["Value"])
     error = float(definition.get("Error", 0.0))
     if error == 0.0:
@@ -74,35 +94,17 @@ def to_parameter(definition: Dict[str, Any]) -> Union[float, Dict[str, float]]:
     return {"Value": value, "Error": error}
 
 
-def to_spin(definition: Dict[str, Any]) -> Union[float, Dict[str, float]]:
-    value = to_scalar(definition)
+def _to_spin(definition: Dict[str, Any]) -> Union[float, Dict[str, float]]:
+    value = _to_scalar(definition)
     if "Projection" in definition:
         return {
             "Value": value,
-            "Projection": to_scalar(definition, "Projection"),
+            "Projection": _to_scalar(definition, "Projection"),
         }
     return value
 
 
-def to_kinematics_dict(recipe: Dict[str, Any]) -> Dict[str, Any]:
-    kinematics_xml = recipe["HelicityKinematics"]
-    initialstate_xml = kinematics_xml["InitialState"]["Particle"]
-    initial_state_yml = list()
-    for state_def_xml in initialstate_xml:
-        state_def_yml = {
-            "Particle": state_def_xml["Name"],
-            "ID": int(state_def_xml["Id"]),
-        }
-        initial_state_yml.append(state_def_yml)
-    kinematics_yaml = {
-        "Type": "Helicity",
-        "InitialState": to_state_list(kinematics_xml, "InitialState"),
-        "FinalState": to_state_list(kinematics_xml, "FinalState"),
-    }
-    return kinematics_yaml
-
-
-def to_state_list(
+def _to_state_list(
     definition: Dict[str, Any], key: str
 ) -> List[Dict[str, Union[str, int]]]:
     state_list_xml = definition[key]["Particle"]
