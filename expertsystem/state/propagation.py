6cbae43532f1e5d3eb1a3e40d6a8e95163b61e45
Remco de Boer <redeboer@gmx.com>
2020-06-24 14:06:32 +0200

    refactor: change LABELS to Labels (Enum class)

--- expertsystem/state/propagation.py
+++ expertsystem/state/propagation.py
@@ -268,9 +268,9 @@ class ParticleStateTransitionGraphValidator(AbstractPropagator):
         Creates variables for the quantum numbers of the specified node.
         """
         variables = {}
-        type_label = particle.LABELS.Type.name
+        type_label = particle.Labels.Type.name
         if node_id in self.graph.node_props:
-            qns_label = particle.LABELS.QuantumNumber.name
+            qns_label = particle.Labels.QuantumNumber.name
             for qn_name in qn_list:
                 converter = QNClassConverterMapping[
                     QNNameClassMapping[qn_name]
@@ -626,7 +626,7 @@ class CSPPropagator(AbstractPropagator):
 def add_qn_to_graph_element(graph, var_info, value):
     if value is None:
         return
-    qns_label = particle.LABELS.QuantumNumber.name
+    qns_label = particle.Labels.QuantumNumber.name
 
     element_id = var_info.element_id
     qn_name = var_info.qn_name
