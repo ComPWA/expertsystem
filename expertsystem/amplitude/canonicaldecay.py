730fe674352dee94285eb5959a0a23b22486639a
Remco de Boer <redeboer@gmx.com>
2020-06-25 10:17:55 +0200

    refactor: convert CG decorator to function

--- expertsystem/amplitude/canonicaldecay.py
+++ expertsystem/amplitude/canonicaldecay.py
@@ -76,6 +76,73 @@ class CanonicalAmplitudeNameGenerator(HelicityAmplitudeNameGenerator):
         return name
 
 
+def _clebsch_gordan_decorator(decay_generate_function):
+    """Decorate a function with Clebsch-Gordan functionality.
+
+    Decorator method which adds two clebsch gordan coefficients based on
+    the translation of helicity amplitudes to canonical ones.
+    """
+
+    def wrapper(self, graph, node_id):
+        spin_type = StateQuantumNumberNames.Spin
+        partial_decay_dict = decay_generate_function(self, graph, node_id)
+        node_props = graph.node_props[node_id]
+        ang_mom = get_interaction_property(
+            node_props, InteractionQuantumNumberNames.L
+        )
+        spin = get_interaction_property(
+            node_props, InteractionQuantumNumberNames.S
+        )
+
+        in_edge_ids = get_edges_ingoing_to_node(graph, node_id)
+
+        parent_spin = get_particle_property(
+            graph.edge_props[in_edge_ids[0]], spin_type
+        )
+
+        daughter_spins = []
+
+        for out_edge_id in get_edges_outgoing_to_node(graph, node_id):
+            daughter_spins.append(
+                get_particle_property(graph.edge_props[out_edge_id], spin_type)
+            )
+
+        decay_particle_lambda = (
+            daughter_spins[0].projection() - daughter_spins[1].projection()
+        )
+        cg_ls = OrderedDict()
+        cg_ls["Type"] = "LS"
+        cg_ls["@j1"] = ang_mom.magnitude()
+        if ang_mom.projection() != 0.0:
+            raise ValueError(
+                "Projection of L is non-zero!: " + str(ang_mom.projection())
+            )
+        cg_ls["@m1"] = ang_mom.projection()
+        cg_ls["@j2"] = spin.magnitude()
+        cg_ls["@m2"] = decay_particle_lambda
+        cg_ls["J"] = parent_spin.magnitude()
+        cg_ls["M"] = decay_particle_lambda
+        cg_ss = OrderedDict()
+        cg_ss["Type"] = "s2s3"
+        cg_ss["@j1"] = daughter_spins[0].magnitude()
+        cg_ss["@m1"] = daughter_spins[0].projection()
+        cg_ss["@j2"] = daughter_spins[1].magnitude()
+        cg_ss["@m2"] = -daughter_spins[1].projection()
+        cg_ss["J"] = spin.magnitude()
+        cg_ss["M"] = decay_particle_lambda
+        cg_dict = {
+            "CanonicalSum": {
+                "L": int(ang_mom.magnitude()),
+                "S": spin.magnitude(),
+                "ClebschGordan": [cg_ls, cg_ss],
+            }
+        }
+        partial_decay_dict.update(cg_dict)
+        return partial_decay_dict
+
+    return wrapper
+
+
 class CanonicalAmplitudeGenerator(HelicityAmplitudeGenerator):
     r"""Amplitude model generator for the canonical helicity formalism.
 
@@ -93,75 +160,6 @@ class CanonicalAmplitudeGenerator(HelicityAmplitudeGenerator):
         name_generator=CanonicalAmplitudeNameGenerator(None),
     ):
         super().__init__(top_node_no_dynamics, name_generator=name_generator)
-
-    def _clebsch_gordan_decorator(decay_generate_function):
-        """Decorate a function with Clebsch-Gordan functionality.
-
-        Decorator method which adds two clebsch gordan coefficients based on
-        the translation of helicity amplitudes to canonical ones.
-        """
-
-        def wrapper(self, graph, node_id):
-            spin_type = StateQuantumNumberNames.Spin
-            partial_decay_dict = decay_generate_function(self, graph, node_id)
-            node_props = graph.node_props[node_id]
-            ang_mom = get_interaction_property(
-                node_props, InteractionQuantumNumberNames.L
-            )
-            spin = get_interaction_property(
-                node_props, InteractionQuantumNumberNames.S
-            )
-
-            in_edge_ids = get_edges_ingoing_to_node(graph, node_id)
-
-            parent_spin = get_particle_property(
-                graph.edge_props[in_edge_ids[0]], spin_type
-            )
-
-            daughter_spins = []
-
-            for out_edge_id in get_edges_outgoing_to_node(graph, node_id):
-                daughter_spins.append(
-                    get_particle_property(
-                        graph.edge_props[out_edge_id], spin_type
-                    )
-                )
-
-            decay_particle_lambda = (
-                daughter_spins[0].projection() - daughter_spins[1].projection()
-            )
-            cg_ls = OrderedDict()
-            cg_ls["Type"] = "LS"
-            cg_ls["@j1"] = ang_mom.magnitude()
-            if ang_mom.projection() != 0.0:
-                raise ValueError(
-                    "Projection of L is non-zero!: "
-                    + str(ang_mom.projection())
-                )
-            cg_ls["@m1"] = ang_mom.projection()
-            cg_ls["@j2"] = spin.magnitude()
-            cg_ls["@m2"] = decay_particle_lambda
-            cg_ls["J"] = parent_spin.magnitude()
-            cg_ls["M"] = decay_particle_lambda
-            cg_ss = OrderedDict()
-            cg_ss["Type"] = "s2s3"
-            cg_ss["@j1"] = daughter_spins[0].magnitude()
-            cg_ss["@m1"] = daughter_spins[0].projection()
-            cg_ss["@j2"] = daughter_spins[1].magnitude()
-            cg_ss["@m2"] = -daughter_spins[1].projection()
-            cg_ss["J"] = spin.magnitude()
-            cg_ss["M"] = decay_particle_lambda
-            cg_dict = {
-                "CanonicalSum": {
-                    "L": int(ang_mom.magnitude()),
-                    "S": spin.magnitude(),
-                    "ClebschGordan": [cg_ls, cg_ss],
-                }
-            }
-            partial_decay_dict.update(cg_dict)
-            return partial_decay_dict
-
-        return wrapper
 
     @_clebsch_gordan_decorator
     def generate_partial_decay(self, graph, node_id):
