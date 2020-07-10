0f3c4359c027f49cff7e90513dce69bf87fb4dfc
Remco de Boer <redeboer@gmx.com>
2020-07-10 14:08:29 +0200

    feat: add JSON schema for helicity YAML recipe

--- expertsystem/io/yaml/validation.py
+++ expertsystem/io/yaml/validation.py
@@ -12,7 +12,13 @@ _PACKAGE_PATH = dirname(realpath(expertsystem.__file__))
 
 with open(f"{_PACKAGE_PATH}/schemas/yaml/particle-list.json") as stream:
     _SCHEMA_PARTICLES = json.load(stream)
+with open(f"{_PACKAGE_PATH}/schemas/yaml/amplitude-model.json") as stream:
+    _SCHEMA_AMPLITUDE = json.load(stream)
 
 
 def particle_list(instance: dict) -> None:
+    jsonschema.validate(instance=instance, schema=_SCHEMA_PARTICLES)
+
+
+def amplitude_model(instance: dict) -> None:
     jsonschema.validate(instance=instance, schema=_SCHEMA_PARTICLES)
