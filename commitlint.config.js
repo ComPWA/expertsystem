05e352cf1845787098a76e7953989b8f7436b759
Remco de Boer <redeboer@gmx.com>
2020-08-03 12:04:26 +0200

    ci: specify allowed commit types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,21 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
-  rules: { "scope-case": [2, "always", "lowerCase"] },
+  rules: {
+    "scope-case": [2, "always", "lowerCase"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "ci",
+        "chore",
+        "docs",
+        "feat",
+        "fix",
+        "refactor",
+        "style",
+        "test",
+      ],
+    ],
+  },
 };
