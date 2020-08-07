27dd5975c82f01afb62bdb62b9b6d370b030c097
sjaeger <sjaeger@ep1.rub.de>
2020-08-07 15:19:06 +0200

    add parametrization for test

--- tests/state/test_particle.py
+++ tests/state/test_particle.py
@@ -1,25 +1,21 @@
+import pytest
+
 from expertsystem.state.particle import DATABASE, create_antiparticle
 from expertsystem.ui import load_default_particle_list
 
+load_default_particle_list()
 
-def test_create_antiparticle():
-    load_default_particle_list()
-    template_particle = DATABASE["D+"]
+
+@pytest.mark.parametrize(
+    "particle_name, anti_particle_name", [("D+", "D-"), ("p", "pbar")],
+)
+def test_create_antiparticle(particle_name, anti_particle_name):
+    template_particle = DATABASE[particle_name]
     anti_particle = create_antiparticle(template_particle)
-    comparision_particle = DATABASE["D-"]
+    comparision_particle = DATABASE[anti_particle_name]
 
     assert anti_particle.pid == comparision_particle.pid
     assert anti_particle.mass == comparision_particle.mass
     assert anti_particle.width == comparision_particle.width
     assert anti_particle.state == comparision_particle.state
-    assert anti_particle.name == "anti-D+"
-
-    template_particle = DATABASE["p"]
-    anti_particle = create_antiparticle(template_particle)
-    comparision_particle = DATABASE["pbar"]
-
-    assert anti_particle.pid == comparision_particle.pid
-    assert anti_particle.mass == comparision_particle.mass
-    assert anti_particle.width == comparision_particle.width
-    assert anti_particle.state == comparision_particle.state
-    assert anti_particle.name == "anti-p"
+    assert anti_particle.name == "anti-" + particle_name
