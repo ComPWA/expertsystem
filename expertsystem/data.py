3a56418a0388e4658ba842e53d143774eaaa7a95
Remco de Boer <redeboer@gmx.com>
2020-08-11 20:26:56 +0200

    feat: check Spin.magnitude mod 0.5 == 0.0

--- expertsystem/data.py
+++ expertsystem/data.py
@@ -64,6 +64,10 @@ class Spin(abc.Hashable):
     def __init__(self, magnitude: float, projection: float) -> None:
         magnitude = float(magnitude)
         projection = float(projection)
+        if magnitude % 0.5 != 0.0:
+            raise ValueError(
+                f"Spin magnitude {magnitude} has to be a multitude of 0.5"
+            )
         if abs(projection) > magnitude:
             raise ValueError(
                 "Spin projection cannot be larger than its magnitude:\n"
