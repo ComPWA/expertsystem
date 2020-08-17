a677a67a9fae11e06c749be76652a4026b1a3a31
Remco de Boer <redeboer@gmx.com>
2020-08-17 14:54:12 +0200

    test: change to package fixture

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -5,7 +5,7 @@ from expertsystem.data import ParticleCollection
 from expertsystem.state.particle import DATABASE
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="package")
 def particle_database() -> ParticleCollection:
     ui.load_default_particle_list()
     return DATABASE
