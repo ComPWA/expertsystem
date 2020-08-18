02b9b50f48b75c8d4c7e41e28f8235f1279f8f96
Remco de Boer <redeboer@gmx.com>
2020-08-18 08:40:08 +0200

    refactor: share test solutions with fixture

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,11 +1,66 @@
+# pylint: disable=redefined-outer-name
+import logging
+
 import pytest
 
 from expertsystem import ui
+from expertsystem.amplitude.canonical_decay import CanonicalAmplitudeGenerator
+from expertsystem.amplitude.helicity_decay import HelicityAmplitudeGenerator
 from expertsystem.data import ParticleCollection
 from expertsystem.state.particle import DATABASE
+from expertsystem.ui import (
+    InteractionTypes,
+    StateTransitionManager,
+)
+
+
+logging.basicConfig(level=logging.ERROR)
 
 
 @pytest.fixture(scope="package")
 def particle_database() -> ParticleCollection:
     ui.load_default_particle_list()
     return DATABASE
+
+
+@pytest.fixture(scope="module")
+def canonical_solutions():
+    stm = StateTransitionManager(
+        initial_state=[("J/psi(1S)", [-1, 1])],
+        final_state=["gamma", "pi0", "pi0"],
+        allowed_intermediate_particles=["f(0)"],
+        formalism_type="canonical-helicity",
+    )
+    stm.set_allowed_interaction_types([InteractionTypes.EM])
+    graph_interaction_settings_groups = stm.prepare_graphs()
+    solutions, _ = stm.find_solutions(graph_interaction_settings_groups)
+    return solutions
+
+
+@pytest.fixture(scope="module")
+def helicity_solutions():
+    stm = StateTransitionManager(
+        initial_state=[("J/psi(1S)", [-1, 1])],
+        final_state=["gamma", "pi0", "pi0"],
+        allowed_intermediate_particles=["f(0)"],
+    )
+    stm.set_allowed_interaction_types(
+        [InteractionTypes.Strong, InteractionTypes.EM]
+    )
+    graph_interaction_settings_groups = stm.prepare_graphs()
+    solutions, _ = stm.find_solutions(graph_interaction_settings_groups)
+    return solutions
+
+
+@pytest.fixture(scope="module")
+def canonical_amplitude_generator(canonical_solutions):
+    amplitude_generator = CanonicalAmplitudeGenerator()
+    amplitude_generator.generate(canonical_solutions)
+    return amplitude_generator
+
+
+@pytest.fixture(scope="module")
+def helicity_amplitude_generator(helicity_solutions):
+    amplitude_generator = HelicityAmplitudeGenerator()
+    amplitude_generator.generate(helicity_solutions)
+    return amplitude_generator
