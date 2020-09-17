9959e79e9ffaaac691c46285906550e8fbcb5735
Remco de Boer <redeboer@gmx.com>
2020-09-17 22:46:45 +0200

    test: initialize_graph with final_state_groupings

--- tests/unit/state/test_graph_combinatorics.py
+++ tests/unit/state/test_graph_combinatorics.py
@@ -1,11 +1,19 @@
+import pytest
+
 from expertsystem.state.particle import initialize_graph
 
 
-def test_initialize_graph(dummy_topology, particle_database):
+@pytest.mark.parametrize(
+    "final_state_groupings", [[[["pi0", "pi0"]]], [[["gamma", "pi0"]]],],
+)
+def test_initialize_graph(
+    final_state_groupings, dummy_topology, particle_database
+):
     graphs = initialize_graph(
         dummy_topology,
         initial_state=[("J/psi(1S)", [-1, +1])],
         final_state=["gamma", "pi0", "pi0"],
         particles=particle_database,
+        final_state_groupings=final_state_groupings,
     )
-    assert len(graphs) == 8
+    assert len(graphs) == 4
