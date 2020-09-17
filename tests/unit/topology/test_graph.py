f63f8a6e958cfa74678f1e10eef4d9922c84ffae
Remco de Boer <redeboer@gmx.com>
2020-09-17 13:36:50 +0200

    test: exceptions InteractionNode
    Fixes patch coverage

--- tests/unit/topology/test_graph.py
+++ tests/unit/topology/test_graph.py
@@ -5,7 +5,40 @@ import pytest
 from expertsystem import io
 from expertsystem import ui
 from expertsystem.state.particle import initialize_graph
-from expertsystem.topology import StateTransitionGraph
+from expertsystem.topology import (
+    InteractionNode,
+    StateTransitionGraph,
+)
+
+
+class TestInteractionNode:
+    @staticmethod
+    def test_constructor_exceptions():
+        dummy_type_name = "type_name"
+        with pytest.raises(TypeError):
+            assert InteractionNode(
+                dummy_type_name,
+                number_of_ingoing_edges="has to be int",  # type: ignore
+                number_of_outgoing_edges=2,
+            )
+        with pytest.raises(TypeError):
+            assert InteractionNode(
+                dummy_type_name,
+                number_of_outgoing_edges="has to be int",  # type: ignore
+                number_of_ingoing_edges=2,
+            )
+        with pytest.raises(ValueError):
+            assert InteractionNode(
+                dummy_type_name,
+                number_of_outgoing_edges=0,
+                number_of_ingoing_edges=1,
+            )
+        with pytest.raises(ValueError):
+            assert InteractionNode(
+                dummy_type_name,
+                number_of_outgoing_edges=1,
+                number_of_ingoing_edges=0,
+            )
 
 
 def create_dummy_topology() -> StateTransitionGraph:
