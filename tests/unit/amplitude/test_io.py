6b4fdc4b5cd6ca8caf6057cbd2b3a9cf443ade42
Remco de Boer <redeboer@gmx.com>
2020-10-05 19:15:04 +0200

    test: verify equivalence XML/YAML
    Closes https://github.com/ComPWA/expertsystem/issues/288

--- tests/unit/amplitude/test_io.py
+++ tests/unit/amplitude/test_io.py
@@ -29,3 +29,32 @@ def test_write_load(
     assert exported.intensity == imported.intensity
     assert exported == imported
     assert exported is not imported
+
+
+@pytest.mark.parametrize("formalism", ["helicity", "canonical"])
+def test_equivalence(
+    formalism: str,
+    jpsi_to_gamma_pi_pi_canonical_amplitude_model: AmplitudeModel,
+    jpsi_to_gamma_pi_pi_helicity_amplitude_model: AmplitudeModel,
+):
+    exported = None
+    if formalism == "helicity":
+        exported = jpsi_to_gamma_pi_pi_helicity_amplitude_model
+    elif formalism == "canonical":
+        exported = jpsi_to_gamma_pi_pi_canonical_amplitude_model
+    else:
+        raise NotImplementedError(formalism)
+    filename = f"test_io_cross_check_{formalism}"
+    filename_xml = f"{filename}.xml"
+    filename_yml = f"{filename}.yml"
+    io.write(exported, filename_xml)
+    io.write(exported, filename_yml)
+    imported_xml = io.load_amplitude_model(filename_xml)
+    imported_yml = io.load_amplitude_model(filename_yml)
+    assert imported_xml.particles == imported_yml.particles
+    assert imported_xml.parameters == imported_yml.parameters
+    assert imported_xml.kinematics == imported_yml.kinematics
+    assert imported_xml.dynamics == imported_yml.dynamics
+    assert imported_xml.intensity == imported_yml.intensity
+    assert imported_xml == imported_yml
+    assert imported_xml is not imported_yml
