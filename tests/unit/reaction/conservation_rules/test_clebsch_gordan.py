61a988ec9811163a7a12238488a7358c2c894d20
Stefan Pflueger <spfluege@gmail.com>
2020-11-10 11:55:42 +0100

    test: add clebsch gordan helicity canonical test

--- tests/unit/reaction/conservation_rules/test_clebsch_gordan.py
+++ tests/unit/reaction/conservation_rules/test_clebsch_gordan.py
@@ -7,6 +7,7 @@ from expertsystem.reaction.conservation_rules import (
     IsoSpinEdgeInput,
     SpinEdgeInput,
     SpinNodeInput,
+    clebsch_gordan_helicity_to_canonical,
     isospin_conservation,
     spin_conservation,
 )
@@ -108,6 +109,92 @@ def test_clebsch_gordan_ls_coupling(
     expected: bool,
 ):
     assert spin_conservation(*rule_input) is expected
+
+
+@pytest.mark.parametrize(
+    "rule_input, expected",
+    [
+        (
+            __create_two_body_decay_spin_data(
+                Spin(1, 0),
+                Spin(1, 1),
+                Spin(1, 1),
+                Spin(0, 0),
+                Spin(1, 0),
+            ),
+            True,
+        ),
+        (
+            __create_two_body_decay_spin_data(
+                Spin(1, 0),
+                Spin(1, 1),
+                Spin(1, -1),
+                Spin(0, 0),
+                Spin(1, 0),
+            ),
+            False,
+        ),
+        (
+            __create_two_body_decay_spin_data(
+                Spin(1, 0),
+                Spin(1, 1),
+                Spin(1, 1),
+                Spin(1, 0),
+                Spin(1, 0),
+            ),
+            False,
+        ),
+        (
+            __create_two_body_decay_spin_data(
+                Spin(1, -1),
+                Spin(1, 0),
+                Spin(1, 1),
+                Spin(1, 0),
+                Spin(1, -1),
+            ),
+            True,
+        ),
+        (
+            __create_two_body_decay_spin_data(
+                Spin(1.5, -0.5),
+                Spin(0.5, 0.5),
+                Spin(1, 1),
+                Spin(1, 0),
+                Spin(0.5, -0.5),
+            ),
+            True,
+        ),
+        (
+            __create_two_body_decay_spin_data(
+                Spin(1.5, -0.5),
+                Spin(0.5, 0.5),
+                Spin(1, 1),
+                Spin(0, 0),
+                Spin(0.5, -0.5),
+            ),
+            True,
+            # This case does actually not work for spin magnitude reasons.
+            # However the spin magnitude rule is already filtering out such,
+            # cases that the Clebsch Gordan rule does not have to implement,
+            # this functionality.
+        ),
+        (
+            __create_two_body_decay_spin_data(
+                Spin(1.5, -0.5),
+                Spin(0.5, 0.5),
+                Spin(1, 1),
+                Spin(0, 0),
+                Spin(1.5, 0.5),
+            ),
+            False,
+        ),
+    ],
+)
+def test_clebsch_gordan_helicity_canonical(
+    rule_input: _SpinRuleInputType,
+    expected: bool,
+):
+    assert clebsch_gordan_helicity_to_canonical(*rule_input) is expected
 
 
 @pytest.mark.parametrize(
