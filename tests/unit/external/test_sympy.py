2ca384eed1b52db99e3c095682a4532e8413d638
Remco de Boer <redeboer@gmx.com>
2021-01-14 09:50:17 +0100

    test: sympy.Symbol.name behavior

--- tests/unit/external/test_sympy.py
+++ tests/unit/external/test_sympy.py
@@ -1,3 +1,4 @@
+# cspell: disable srepr
 # pylint: disable=invalid-name,no-self-use
 
 import sympy as sy
@@ -10,3 +11,24 @@ class TestSymbol:
         assert x == y
         assert x is y
         assert hash(x) == hash(y)
+
+    def test_name(self):
+        # pylint: disable=no-member
+        x = sy.Symbol("x; weird-spacing\t.,")
+        f = sy.Function("  f.^")
+        g = sy.Function("g")(x)
+        assert x.name == "x; weird-spacing	.,"
+        assert sy.srepr(x) == "Symbol('x; weird-spacing\\t.,')"
+        assert f.name == "  f.^"
+        assert g.name == "g"
+        x.name = "x"
+        assert x.name == "x"
+
+    def test_name_change(self):
+        x = sy.Symbol("a")
+        y = sy.Symbol("a")
+        assert x.name == y.name
+        assert x == y
+        assert x is y
+        x.name = "I am x"
+        assert y.name == "I am x"
