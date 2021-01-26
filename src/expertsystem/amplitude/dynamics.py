831083c04b0ba5f029759da3fced8e8e8f50fb81
Stefan Pflueger <spfluege@gmail.com>
2021-01-26 15:25:52 +0100

    fix: revert SuggestedParameterValues to attr class

--- src/expertsystem/amplitude/dynamics.py
+++ src/expertsystem/amplitude/dynamics.py
@@ -4,26 +4,26 @@ import sympy as sy
 
 
 def hankel1(angular_momentum: sy.Symbol, x: sy.Symbol) -> sy.Expr:
-    x_squared = x ** 2
+    x_sq = x ** 2
     return sy.Piecewise(
         (
             1,
             sy.Eq(angular_momentum, 0),
         ),
         (
-            1 + x_squared,
+            1 + x_sq,
             sy.Eq(angular_momentum, 1),
         ),
         (
-            9 + x_squared * (3 + x_squared),
+            9 + x_sq * (3 + x_sq),
             sy.Eq(angular_momentum, 2),
         ),
         (
-            225 + x_squared * (45 + x_squared * (6 + x_squared)),
+            225 + x_sq * (45 + x_sq * (6 + x_sq)),
             sy.Eq(angular_momentum, 3),
         ),
         (
-            1575 + x_squared * (135 + x_squared * (10 + x_squared)),
+            11025 + x_sq * (1575 + x_sq * (135 + x_sq * (10 + x_sq))),
             sy.Eq(angular_momentum, 4),
         ),
     )
@@ -33,15 +33,14 @@ def blatt_weisskopf(
     q: sy.Symbol, q_r: sy.Symbol, angular_momentum: sy.Symbol
 ) -> sy.Expr:
     return sy.sqrt(
-        abs(hankel1(angular_momentum, q)) ** 2
-        / abs(hankel1(angular_momentum, q_r)) ** 2
+        hankel1(angular_momentum, q) / hankel1(angular_momentum, q_r)
     )
 
 
 def relativistic_breit_wigner(
     mass: sy.Symbol, mass0: sy.Symbol, gamma0: sy.Symbol
 ) -> sy.Expr:
-    return gamma0 / (mass0 ** 2 - mass ** 2 - gamma0 * mass0 * sy.I)
+    return gamma0 * mass0 / (mass0 ** 2 - mass ** 2 - gamma0 * mass0 * sy.I)
 
 
 def relativistic_breit_wigner_with_form_factor(  # pylint: disable=too-many-arguments
@@ -59,12 +58,7 @@ def relativistic_breit_wigner_with_form_factor(  # pylint: disable=too-many-argu
     ff02 = blatt_weisskopf(q0_squared, meson_radius, angular_momentum)
     width = gamma0 * (mass0 / mass) * (ff2 / ff02)
     width = width * sy.sqrt(q_squared / q0_squared)
-    return (
-        relativistic_breit_wigner(mass, mass0, width)
-        * mass0
-        * gamma0
-        * sy.sqrt(ff2)
-    )
+    return relativistic_breit_wigner(mass, mass0, width) * sy.sqrt(ff2)
 
 
 def two_body_momentum_squared(
