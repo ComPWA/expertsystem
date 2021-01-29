1e8f04f9ea694de43785a6e41eb5f8c16ce4ff4c
Remco de Boer <redeboer@gmx.com>
2021-01-29 14:06:47 +0100

    docs: add intersphinx support for sympy

--- docs/conf.py
+++ docs/conf.py
@@ -179,6 +179,7 @@ intersphinx_mapping = {
     "pwa": ("https://pwa.readthedocs.io", None),
     "pycompwa": ("https://compwa.github.io", None),
     "python": ("https://docs.python.org/3", None),
+    "sympy": ("https://docs.sympy.org/latest", None),
     "tensorwaves": (
         "https://pwa.readthedocs.io/projects/tensorwaves/en/stable",
         None,
