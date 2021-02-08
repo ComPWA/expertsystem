1f5a2615c5475856ea20cd9c0187684b476a74cf
Remco de Boer <redeboer@gmx.com>
2021-02-08 17:43:39 +0100

    fix: use alphabetical order for tests

--- tests/unit/reaction/test_combinatorics.py
+++ tests/unit/reaction/test_combinatorics.py
@@ -31,66 +31,6 @@ def three_body_decay() -> Topology:
     return topology
 
 
-@pytest.mark.parametrize(
-    "final_state_groupings",
-    [
-        ["pi0", "pi0"],
-        [["pi0", "pi0"]],
-        [[["pi0", "pi0"]]],
-        ["gamma", "pi0"],
-        [["gamma", "pi0"]],
-        [[["gamma", "pi0"]]],
-    ],
-)
-def test_create_initial_facts(
-    final_state_groupings,
-    three_body_decay: Topology,
-    particle_database: ParticleCollection,
-):
-    initial_facts = create_initial_facts(
-        three_body_decay,
-        initial_state=[("J/psi(1S)", [-1, +1])],
-        final_state=["gamma", "pi0", "pi0"],
-        particles=particle_database,
-        final_state_groupings=final_state_groupings,
-    )
-    assert len(initial_facts) == 4
-
-
-@pytest.mark.parametrize(
-    "initial_state, final_state",
-    [
-        (["J/psi(1S)"], ["gamma", "pi0", "pi0"]),
-        (["J/psi(1S)"], ["K+", "K-", "pi+", "pi-"]),
-        (["e+", "e-"], ["gamma", "pi-", "pi+"]),
-        (["e+", "e-"], ["K+", "K-", "pi+", "pi-"]),
-    ],
-)
-def test_generate_outer_edge_permutations(
-    initial_state,
-    final_state,
-    three_body_decay: Topology,
-    particle_database: ParticleCollection,
-):
-    initial_state_with_spins = _safe_set_spin_projections(
-        initial_state, particle_database
-    )
-    final_state_with_spins = _safe_set_spin_projections(
-        final_state, particle_database
-    )
-    list_of_permutations = list(
-        _generate_outer_edge_permutations(
-            three_body_decay,
-            initial_state_with_spins,
-            final_state_with_spins,
-        )
-    )
-    n_permutations_final_state = factorial(len(final_state))
-    n_permutations_initial_state = factorial(len(initial_state))
-    n_permutations = n_permutations_final_state * n_permutations_initial_state
-    assert len(list_of_permutations) == n_permutations
-
-
 class TestKinematicRepresentation:
     @staticmethod
     def test_constructor():
@@ -164,6 +104,66 @@ class TestKinematicRepresentation:
             assert float() in kinematic_representation
         with pytest.raises(ValueError):
             assert ["should be nested list"] in kinematic_representation
+
+
+@pytest.mark.parametrize(
+    "final_state_groupings",
+    [
+        ["pi0", "pi0"],
+        [["pi0", "pi0"]],
+        [[["pi0", "pi0"]]],
+        ["gamma", "pi0"],
+        [["gamma", "pi0"]],
+        [[["gamma", "pi0"]]],
+    ],
+)
+def test_create_initial_facts(
+    final_state_groupings,
+    three_body_decay: Topology,
+    particle_database: ParticleCollection,
+):
+    initial_facts = create_initial_facts(
+        three_body_decay,
+        initial_state=[("J/psi(1S)", [-1, +1])],
+        final_state=["gamma", "pi0", "pi0"],
+        particles=particle_database,
+        final_state_groupings=final_state_groupings,
+    )
+    assert len(initial_facts) == 4
+
+
+@pytest.mark.parametrize(
+    "initial_state, final_state",
+    [
+        (["J/psi(1S)"], ["gamma", "pi0", "pi0"]),
+        (["J/psi(1S)"], ["K+", "K-", "pi+", "pi-"]),
+        (["e+", "e-"], ["gamma", "pi-", "pi+"]),
+        (["e+", "e-"], ["K+", "K-", "pi+", "pi-"]),
+    ],
+)
+def test_generate_outer_edge_permutations(
+    initial_state,
+    final_state,
+    three_body_decay: Topology,
+    particle_database: ParticleCollection,
+):
+    initial_state_with_spins = _safe_set_spin_projections(
+        initial_state, particle_database
+    )
+    final_state_with_spins = _safe_set_spin_projections(
+        final_state, particle_database
+    )
+    list_of_permutations = list(
+        _generate_outer_edge_permutations(
+            three_body_decay,
+            initial_state_with_spins,
+            final_state_with_spins,
+        )
+    )
+    n_permutations_final_state = factorial(len(final_state))
+    n_permutations_initial_state = factorial(len(initial_state))
+    n_permutations = n_permutations_final_state * n_permutations_initial_state
+    assert len(list_of_permutations) == n_permutations
 
 
 def test_generate_permutations(
