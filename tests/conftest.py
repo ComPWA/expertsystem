06f7469abf175307d8506a8025a23345672ac514
Remco de Boer <redeboer@gmx.com>
2021-02-08 10:53:58 +0100

    Revert "test: perform_external_edge_identical_particle_combinatorics"
    This reverts commit 123fe46a32be1bbd8c3c45d25405e52514507b39.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,7 @@
-from typing import Callable
-
 import pytest
 
 from expertsystem.particle import ParticleCollection
-from expertsystem.reaction import (
-    Result,
-    StateTransitionManager,
-    load_default_particles,
-)
-from expertsystem.reaction.default_settings import InteractionTypes
+from expertsystem.reaction import load_default_particles
 
 
 @pytest.fixture(scope="session")
@@ -19,21 +12,3 @@ def particle_database() -> ParticleCollection:
 @pytest.fixture(scope="session")
 def output_dir(pytestconfig) -> str:
     return f"{pytestconfig.rootpath}/tests/output/"
-
-
-@pytest.fixture(scope="session")
-def y_to_d0_d0bar_pi0_pi0() -> Callable[[str], Result]:
-    def _y_to_d0_d0bar_pi0_pi0(formalism: str) -> Result:
-        stm = StateTransitionManager(
-            initial_state=[("Y(4260)", [-1, +1])],
-            final_state=["D0", "D~0", "pi0", "pi0"],
-            allowed_intermediate_particles=["D*"],
-            formalism_type=formalism,
-            number_of_threads=1,
-        )
-        stm.set_allowed_interaction_types([InteractionTypes.Strong])
-        stm.add_final_state_grouping([["D0", "pi0"], ["D~0", "pi0"]])
-        problem_sets = stm.create_problem_sets()
-        return stm.find_solutions(problem_sets)
-
-    return _y_to_d0_d0bar_pi0_pi0
