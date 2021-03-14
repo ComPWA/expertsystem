9ccd5c4e36fe0f3177f396e2ec774e8878dcaf6b
Remco de Boer <redeboer@gmx.com>
2021-03-14 21:24:52 +0100

    test: assert type out of fromdict

--- tests/unit/io/test_io.py
+++ tests/unit/io/test_io.py
@@ -1,16 +1,20 @@
 import pytest
 
 from expertsystem import io
-from expertsystem.particle import ParticleCollection
+from expertsystem.particle import Particle, ParticleCollection
 
 
 def test_asdict_fromdict(particle_selection: ParticleCollection):
+    # ParticleCollection
     asdict = io.asdict(particle_selection)
     fromdict = io.fromdict(asdict)
+    assert isinstance(fromdict, ParticleCollection)
     assert particle_selection == fromdict
+    # Particle
     for particle in particle_selection:
         asdict = io.asdict(particle)
         fromdict = io.fromdict(asdict)
+        assert isinstance(fromdict, Particle)
         assert particle == fromdict
 
 
