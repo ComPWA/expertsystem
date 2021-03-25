5772db0c0884d84bee0a7cc00afc1a510e98fa6b
Remco de Boer <redeboer@gmx.com>
2021-03-25 18:11:09 +0100

    ci: remove check from fix-first-nbcell
    Always perform autofix

--- .pre-commit-hooks/fix_first_nbcell.py
+++ .pre-commit-hooks/fix_first_nbcell.py
@@ -12,10 +12,8 @@ to SVG. This is because the Sphinx configuration can't set this externally.
 
 import argparse
 import configparser
-import json
 import sys
-import textwrap
-from typing import List, Optional, Sequence
+from typing import Optional, Sequence
 
 import nbformat  # type: ignore
 
@@ -39,26 +37,6 @@ EXPECTED_CELL_METADATA = {
     "slideshow": {"slide_type": "skip"},
     "tags": ["remove-cell"],
 }
-
-
-class WrongCellContent(ValueError):
-    pass
-
-
-class WrongMetadata(ValueError):
-    pass
-
-
-def check_first_cell(filename: str, expected_content: str) -> None:
-    notebook = nbformat.read(filename, as_version=4)
-    first_cell = notebook.cells[0]
-    cell_content = "".join(first_cell.source)
-    cell_content = cell_content.strip("\n")
-    expected_cell_content = expected_content.strip("\n")
-    if cell_content != expected_cell_content:
-        raise WrongCellContent(filename)
-    if first_cell.metadata != EXPECTED_CELL_METADATA:
-        raise WrongMetadata(filename)
 
 
 def fix_first_cell(
@@ -87,11 +65,6 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
         help="Additional packages to be installed through the first notebook cell",
     )
     parser.add_argument(
-        "--autofix",
-        action="store_true",
-        help="Automatically fix first cell in the notebook.",
-    )
-    parser.add_argument(
         "--replace",
         action="store_true",
         help="Replace first cell instead of prepending a new cell.",
@@ -104,37 +77,12 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
         expected_cell_content += f"\n!{{sys.executable}} -m pip freeze | grep {package} || {{sys.executable}} -m pip install {package}"
 
     exit_code = 0
-    wrong_content_files: List[str] = []
-    wrong_metadata_files: List[str] = []
     for filename in args.filenames:
-        try:
-            check_first_cell(filename, expected_cell_content)
-        except ValueError as exception:
-            if args.autofix:
-                fix_first_cell(filename, expected_cell_content, args.replace)
-            if isinstance(exception, WrongCellContent):
-                wrong_content_files.append(filename)
-            if isinstance(exception, WrongMetadata):
-                wrong_metadata_files.append(filename)
-            exit_code = 1
-    if exit_code != 0 and not args.autofix:
-        if wrong_content_files or wrong_metadata_files:
-            print("First cells in notebooks should follow a certain standard.")
-            if wrong_content_files:
-                print("Cell content problems for notebooks:")
-                print(" ", *wrong_content_files, sep="\n ")
-                print("\nCell content should be:")
-                print(textwrap.indent(EXPECTED_CELL_CONTENT, prefix="  "))
-            if wrong_metadata_files:
-                print("Metadata problems for notebooks:")
-                print(" ", *wrong_metadata_files, sep="\n ")
-                print("\nMetadata should be:\n")
-                print(
-                    textwrap.indent(
-                        json.dumps(EXPECTED_CELL_METADATA, indent=2),
-                        prefix="  ",
-                    )
-                )
+        fix_first_cell(
+            filename,
+            new_content=expected_cell_content,
+            replace=args.replace,
+        )
     return exit_code
 
 
